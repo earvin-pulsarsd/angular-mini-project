cffd76a8e40bb76902a38e690e53e217c8111fd5
Earvin Leonardo <earvin.leonardo@pulsar-sd.com>
2020-10-15 14:15:45 +0800

    modified user service

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private usersUrl = 'https://jsonplaceholder.typicode.com/users';
+  users$ = this.http.get<User[]>(this.usersUrl);
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
