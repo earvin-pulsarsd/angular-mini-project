d949dbd24ba07bd9a56547c4cfbb039c391dd1a3
Earvin Leonardo <earvin.leonardo@pulsar-sd.com>
2020-10-21 14:46:33 +0800

    added user posts with pagination

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from '../user.service';
 import { Observable } from 'rxjs';
@@ -6,6 +6,8 @@ import { User } from '../user';
 import * as L from 'leaflet';
 import { Post } from '../post';
 import { map } from 'rxjs/operators';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-user-details',
@@ -13,8 +15,11 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./user-details.component.scss']
 })
 export class UserDetailsComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  displayedColumns: string[] = ['title', 'body'];
+  dataSource: MatTableDataSource<Post>;
 
-  isEditable: boolean = true;
+  isNotEditable: boolean = true;
   id: number;
   users: Observable<User[]>;
   user: Observable<User>;
@@ -22,19 +27,7 @@ export class UserDetailsComponent implements OnInit, AfterViewInit, OnDestroy {
   private map;
 
   constructor(private userService: UserService, private route: ActivatedRoute) { }
-  private initMap(): void {
-    this.map = L.map('map', {
-      center: [ 39.8282, -98.5795 ],
-      zoom: 3
-    });
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
-
-    tiles.addTo(this.map);
-  }
-
+  
   ngOnInit(): void {
     this.id = +this.route.snapshot.paramMap.get('id');
     this.users = this.userService.users$;
@@ -48,14 +41,31 @@ export class UserDetailsComponent implements OnInit, AfterViewInit, OnDestroy {
   }
   updateForm(id: number): void {
     this.user = this.userService.getUser(id);
+    this.posts = this.userService.posts$.pipe(map(posts => posts.filter(posts => posts.userId === id)));
     this.user.subscribe(user => {this.map.setView([user.address.geo.lat, user.address.geo.lng], 3), L.marker([user.address.geo.lat, user.address.geo.lng]).addTo(this.map)});
+    this.posts.subscribe(posts => {
+      this.dataSource = new MatTableDataSource(posts);
+      this.dataSource.paginator = this.paginator;
+    });
   }
   toggleEdit(): void {
-    if(this.isEditable) {
-      this.isEditable = false;
+    if(this.isNotEditable) {
+      this.isNotEditable = false;
     }
     else {
-      this.isEditable = true;
+      this.isNotEditable = true;
     }
+  }
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 39.8282, -98.5795 ],
+      zoom: 3
+    });
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+  
+    tiles.addTo(this.map);
   }
 }
